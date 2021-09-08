27648973c0d134a97c346825d739719efac5c378
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-09-08 23:54:44 +0900

    stack_oop

--- oop_project/stack.ts
+++ oop_project/stack.ts
@@ -15,9 +15,11 @@
         
         private _size: number = 0;
         private head?: StackNode;
+        private _capacity: number = 10;
 
         constructor(private capacity: number) {
-           
+            this._capacity = capacity;
+            if (this._capacity > 10) throw new Error(`${this.capacity}이하로 스택을 만들어줄래요~?`);
         }
 
         get size() {
@@ -26,8 +28,11 @@
 
         push(value: string): void {
             if (this.size === this.capacity) {
-                throw new Error('stack is full');
+                throw new Error(`${this.capacity}이하로 스택을 만들어줄래요~?`);
             }
+            // node는 새로 들어온 요소 그리고 head는 node
+            // next는 현재 head
+            // 그리고 현재 head는 새로 들어온 node가 된다!
             const node: StackNode = {value: value , next: this.head} // 새로 들어오는 요소
             this.head = node; // 헤드는 새로들어 오는 요소를 가르킴 last in last out
             this._size++; // 사이즈 증가
@@ -36,7 +41,9 @@
 
             if (this.head == null) {
                 throw new Error('stack is empty'); // 스택이 비어있을때 에러
-             }
+            }
+            // 현재의 head는 next가 되고
+            // 현재 head의 value를 return
             const node = this.head;
             this.head = node.next;
             this._size--
@@ -47,7 +54,7 @@
 
     }
 
-    const stack = new StackImpl(10);
+    const stack = new StackImpl(11);
     stack.push('mac');
     stack.push('os');
     stack.push('linux');
