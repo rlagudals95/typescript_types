43213c72e761232649c2c0768acc104a0a731ecb
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-09-15 22:16:29 +0900

    tsconfig

---
+++ config/src/logging/logging.ts
@@ -0,0 +1,8 @@
+//  tsc logging.ts -w 로 저장할때 마다 js 파일로 자동 컴파일
+//  tsc --init > tsconfig.json 생성
+//  tsc -w 를 터미널에 입력하면 tsconfig.json이 있는 프로젝트 내의 ts파일은 자동으로 js로 컴파일 된다
+console.log('hello')
+console.log('hi')
+
+const number: number = 1;
+const string: string = 'hmk'
